e95ae8b457573bbd7e3760b0c20d6c8943855a2a
Sum Liu <1285370080@qq.com>
2018-12-20 15:26:22 +0800

    首页更新
    首页内容更新

--- data/data_index.js
+++ data/data_index.js
@@ -5,108 +5,127 @@ module.exports = {
 
 }
 
-var index= {
-    "id": 1,
-        "data": [
-        {
-            "question_id": 1,
-            "answer_id": 3,
-            "feed_source_id": 23,
-            "feed_source_name": "Rebecca",
-            "feed_source_txt": "赞了回答1",
-            "feed_source_img": "../../images/icon1.jpeg",
-            "question": "选择 Kindle 而不是纸质书的原因是什么？",
-            "answer_ctnt": "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了...",
-            "good_num": "112",
-            "comment_num": "18"
-        },
-        {
-            "question_id": 2,
-            "answer_id": 25,
-            "feed_source_id": 24,
-            "feed_source_name": "Alex",
-            "feed_source_txt": "回答了问题2",
-            "feed_source_img": "../../images/icon8.jpg",
-            "question": "如何评价周杰伦的「中文歌才是最屌的」的言论？",
-            "answer_ctnt": "不知道题主是否是学音乐的。 音乐有公认的经典，也有明显的流行趋势没有错。但归根结底，音乐是一种艺术，艺术是很主观的东西。跟画作一个道理，毕加索是大家，但很多人看不懂他的话，甚至觉得很难看...",
-            "good_num": "112",
-            "comment_num": "18"
-        },
-        {
-            "question_id": 3,
-            "answer_id": 61,
-            "feed_source_id": 25,
-            "feed_source_name": "George",
-            "feed_source_txt": "赞了回答3",
-            "feed_source_img": "../../images/icon9.jpeg",
-            "question": "气象铁塔的辐射大吗？",
-            "answer_ctnt": "我不知道那个铁塔的情况，不过气象铁塔上会有一些测太阳辐射的设备，如果说辐射的话，太阳辐射那么多，大家赶紧躲进地底下呀~~~~~要不然辐射量这么大，会变异的呀~~~~",
-            "good_num": "112",
-            "comment_num": "18"
-        },
-        {
-            "question_id": 4,
-            "answer_id": 3,
-            "feed_source_id": 23,
-            "feed_source_name": "Rebecca",
-            "feed_source_txt": "赞了回答4",
-            "feed_source_img": "../../images/icon1.jpeg",
-            "question": "选择 Kindle 而不是纸质书的原因是什么？",
-            "answer_ctnt": "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了...",
-            "good_num": "112",
-            "comment_num": "18"
-        },
-        {
-            "question_id": 5,
-            "answer_id": 25,
-            "feed_source_id": 24,
-            "feed_source_name": "Alex",
-            "feed_source_txt": "回答了问题5",
-            "feed_source_img": "../../images/icon8.jpg",
-            "question": "如何评价周杰伦的「中文歌才是最屌的」的言论？",
-            "answer_ctnt": "不知道题主是否是学音乐的。 音乐有公认的经典，也有明显的流行趋势没有错。但归根结底，音乐是一种艺术，艺术是很主观的东西。跟画作一个道理，毕加索是大家，但很多人看不懂他的话，甚至觉得很难看...",
-            "good_num": "112",
-            "comment_num": "18"
-        },
-        {
-            "question_id": 6,
-            "answer_id": 61,
-            "feed_source_id": 25,
-            "feed_source_name": "George",
-            "feed_source_txt": "赞了回答6",
-            "feed_source_img": "../../images/icon9.jpeg",
-            "question": "气象铁塔的辐射大吗？",
-            "answer_ctnt": "我不知道那个铁塔的情况，不过气象铁塔上会有一些测太阳辐射的设备，如果说辐射的话，太阳辐射那么多，大家赶紧躲进地底下呀~~~~~要不然辐射量这么大，会变异的呀~~~~",
-            "good_num": "112",
-            "comment_num": "18"
-        },
-        {
-            "question_id": 7,
-            "answer_id": 3,
-            "feed_source_id": 23,
-            "feed_source_name": "Rebecca",
-            "feed_source_txt": "赞了回答7",
-            "feed_source_img": "../../images/icon1.jpeg",
-            "question": "选择 Kindle 而不是纸质书的原因是什么？",
-            "answer_ctnt": "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了...",
-            "good_num": "112",
-            "comment_num": "18"
-        },
-        {
-            "question_id": 8,
-            "answer_id": 25,
-            "feed_source_id": 24,
-            "feed_source_name": "Alex",
-            "feed_source_txt": "回答了问题8",
-            "feed_source_img": "../../images/icon8.jpg",
-            "question": "如何评价周杰伦的「中文歌才是最屌的」的言论？",
-            "answer_ctnt": "不知道题主是否是学音乐的。 音乐有公认的经典，也有明显的流行趋势没有错。但归根结底，音乐是一种艺术，艺术是很主观的东西。跟画作一个道理，毕加索是大家，但很多人看不懂他的话，甚至觉得很难看...",
-            "good_num": "112",
-            "comment_num": "18"
-        }
+var list = [
+  {
+    id: 9,
+    star: 1231341,
+    name: '蒋佩熙',
+    pic: '../../images/detail1.jpg'
+  },
+  {
+    id: 6,
+    star: 1211341,
+    name: '刘奕菲',
+    pic: '../../images/detail2.jpg'
+  },
+  {
+    id: 56,
+    star: 931341,
+    name: '唐子惠',
+    pic: '../../images/detail3.jpg'
+  },
+  {
+    id: 3,
+    star: 131341,
+    name: '邹馨儿',
+    pic: '../../images/detail4.jpg'
+  },
+  {
+    id: 14,
+    star: 56456,
+    name: '陶小宝',
+    pic: '../../images/detail5.jpg'
+  },
+  {
+    id: 39,
+    star: 30867,
+    name: '黄佳欣',
+    pic: '../../images/detail6.jpg'
+  },
+  {
+    id: 32,
+    star: 29439,
+    name: '张艺严',
+    pic: '../../images/detail7.jpg'
+  },
+  {
+    id: 21,
+    star: 26559,
+    name: '杨佳欣',
+    pic: '../../images/detail8.jpg'
+  },
+  {
+    id: 35,
+    star: 27212,
+    name: '周雨绮',
+    pic: '../../images/detail9.jpg'
+  },
+  {
+    id: 15,
+    star: 26331,
+    name: '周晨',
+    pic: '../../images/detail10.jpg'
+  },
+  {
+    id: 4,
+    star: 23036,
+    name: '张馨月',
+    pic: '../../images/detail11.jpg'
+  },
+  {
+    id: 28,
+    star: 24892,
+    name: '陈恩童',
+    pic: '../../images/detail12.jpg'
+  },
+  {
+    id: 52,
+    star: 23456,
+    name: '谭皓航',
+    pic: '../../images/detail13.jpg'
+  },
+  {
+    id: 52,
+    star: 22023,
+    name: '王美臻',
+    pic: '../../images/detail14.jpg'
+  },
+  {
+    id: 67,
+    star: 20968,
+    name: '恬恬',
+    pic: '../../images/detail15.jpg'
+  },
+  {
+    id: 40,
+    star: 16984,
+    name: '刘好',
+    pic: '../../images/detail16.jpg'
+  },
+  {
+    id: 58,
+    star: 56456,
+    name: '钟湘豫',
+    pic: '../../images/detail17.jpg'
+  },
+  {
+    id: 66,
+    star: 56456,
+    name: '雷蕾',
+    pic: '../../images/detail18.jpg'
+  },
+  {
+    id: 36,
+    star: 8832,
+    name: '李子怡',
+    pic: '../../images/detail19.jpg'
+  },
+  {
+    id: 12,
+    star: 9487,
+    name: '李黛依',
+    pic: '../../images/detail20.jpg'
+  },
+]
 
-    ]
-
-}
-
-module.exports.index = index;
+module.exports.list = list;
